e95d6d95cdf1c02681202c206e8a7545016f8b35
ljsikuade <40580944+ljsikuade@users.noreply.github.com>
2018-09-04 15:48:11 +0100

    Activities completed on 04/09/2018.

--- src/fundamentals.js
+++ src/fundamentals.js
@@ -1,81 +1,123 @@
-function add(a, b){
+function add(a, b) {
   // add the two arguments and return the sum
   return a + b;
 }
 
 // Rewrite function as multiline arrow function with return statement
-function addArrow(a, b){
+const addArrow = (a, b) => {
   // add the two arguments and return the sum
-}
+  return a + b;
+};
 
 // Rewrite function as single line arrow function without a return statement
-function addSingleLineArrow(a, b){
-  // add the two arguments and return the sum
-}
+const addSingleLineArrow = (a, b) => a + b;
+// add the two arguments and return the sum
 
 // Rewrite function as arrow function
-function remainder(number, divisor){
-  // return the remainder of number
-  // when divided by divisor
-}
+const remainder = (number, divisor) => number % divisor;
 
 // Rewrite function as arrow function
-function exponential(number, exponent){
-  // this function is passed 2 arguments
-  // return first argument to the power of second argument
-  // hint: you may need to look up the exponention operator
-}
+const exponential = (number, exponent) => Math.pow(number, exponent);
+// this function is passed 2 arguments
+// return first argument to the power of second argument
+// hint: you may need to look up the exponention operator
 
 // Rewrite function as arrow function
-function smaller(a, b){
-  // function receives two numbers as arguments
-  // return smaller number
-  // if numbers are the same return `false`
-}
+const smaller = (a, b) => {
+  if (a === b) {
+    return false;
+  } else if (a > b) {
+    return b;
+  }
+  return a;
+};
+// function receives two numbers as arguments
+// return smaller number
+// if numbers are the same return `false`
 
 // Rewrite function as arrow function
-function circumference(diameter){
-  // function receives the diameter
-  // calculate the circumference of the circle
-  // return number as a string with 3 decimal places
-}
+const circumference = diameter => (Math.PI * diameter).toFixed(3);
+// function receives the diameter
+// calculate the circumference of the circle
+// return number as a string with 3 decimal places
 
 // Rewrite function as arrow function
-function countOnes(onesAndZeros){
+const countOnes = onesAndZeros => {
+  let ones = 0;
+  for (let i = 0; i < onesAndZeros.length; i++) {
+    if (onesAndZeros.charAt(i) === "1") {
+      ones++;
+      console.log(onesAndZeros.charAt(i));
+    }
+  }
+  return ones;
   // onesAndZeros is a string which contains only `1` and `0`
-
   // return the number of `1`s in the string
-}
+};
 
 // Rewrite function as arrow function
-function dateFormat(date){
+const dateFormat = date => {
+  const months = {
+    "01": "January",
+    "02": "February",
+    "03": "March",
+    "04": "April",
+    "05": "May",
+    "06": "June",
+    "07": "July",
+    "08": "August",
+    "09": "September",
+    "10": "October",
+    "11": "November",
+    "12": "December"
+  };
+
+  let dateArr = date.split("-");
+  //"03","11","2018"
+  let day = parseInt(dateArr[0]);
+  let month = months[dateArr[1]];
+  let year = dateArr[2];
+  return `${day} ${month} ${year}`;
+
   // function receives a date as a string
   // in the format yyyy-mm-dd
-
   // return date in format d mmmm yyyy
   // for example '03-11-2018' becomes '3 November 2018'
-
   // hint: you may want to use the string method `split`.
-}
+};
 
 // Rewrite function as arrow function
-function mulitplyAll(numbers){
+const mulitplyAll = numbers => {
+  const reducer = (accumulator, currentValue) => accumulator * currentValue;
+  console.log(accumulator);
+  return numbers.reduce(reducer);
+
   // numbers is an array of numbers
   // multiply all numbers in array
   // and return the product
-}
+};
 
 // Rewrite function as arrow function
-function largestArea(rectangles){
+const largestArea = rectangles => {
   // rectangles is an array of arrays,
   // where the inner contains two numbers
   // the first number is the height of a rectangle and
   // the second number is the width of a rectangle
-
   // for example [[4,2], [7, 1], [5, 5]] return 25
-
   // return the area of the largest rectangle
-}
+  // let areas = rectangles
+  //   .map(x => x[0] * x[1])
+  //   .sort(function compareNumbers(a, b) {
+  //     return b - a;
+  //   });
+  // return areas[0];
+
+  rectangles.reduce(function(biggest, rec) {
+    // find area of rec
+    // if area > biggest
+    // biggest = area
+  }, 1); // initialise biggest as 1
+};
 
 module.exports = {
   add,
--- test/fundamentals.test.js
+++ test/fundamentals.test.js
@@ -10,142 +10,142 @@ const {
   dateFormat,
   mulitplyAll,
   largestArea
-} = require('../src/fundamentals');
+} = require("../src/fundamentals");
 
-test('add - 1', () => {
-  const result = add( 2, 3 );
+test("add - 1", () => {
+  const result = add(2, 3);
 
-  expect( result ).toEqual( 5 );
+  expect(result).toEqual(5);
 });
 
-test('add - 2', () => {
-  const result = add( 7, 2 );
+test("add - 2", () => {
+  const result = add(7, 2);
 
-  expect( result ).toEqual( 9 );
+  expect(result).toEqual(9);
 });
 
-test('addArrow - 1', () => {
-  const result = addArrow( 1, 6 );
+test("addArrow - 1", () => {
+  const result = addArrow(1, 6);
 
-  expect( result ).toEqual( 7 );
+  expect(result).toEqual(7);
 });
 
-test('addArrow - 2', () => {
-  const result = addArrow( 4, 7 );
+test("addArrow - 2", () => {
+  const result = addArrow(4, 7);
 
-  expect( result ).toEqual( 11 );
+  expect(result).toEqual(11);
 });
 
-test('addSingleLineArrow - 1', () => {
-  const result = addSingleLineArrow( 5, 5 );
+test("addSingleLineArrow - 1", () => {
+  const result = addSingleLineArrow(5, 5);
 
-  expect( result ).toEqual( 10 );
+  expect(result).toEqual(10);
 });
 
-test('addSingleLineArrow - 2', () => {
-  const result = addSingleLineArrow( 4, 8 );
+test("addSingleLineArrow - 2", () => {
+  const result = addSingleLineArrow(4, 8);
 
-  expect( result ).toEqual( 12 );
+  expect(result).toEqual(12);
 });
 
-test('remainder - 1', () => {
-  const result = remainder( 14, 3 );
+test("remainder - 1", () => {
+  const result = remainder(14, 3);
 
-  expect( result ).toEqual( 2 );
+  expect(result).toEqual(2);
 });
 
-test('remainder - 2', () => {
-  const result = remainder( 27, 8 );
+test("remainder - 2", () => {
+  const result = remainder(27, 8);
 
-  expect( result ).toEqual( 3 );
+  expect(result).toEqual(3);
 });
 
-test('exponential - 1', () => {
-  const result = exponential( 4, 3 );
+test("exponential - 1", () => {
+  const result = exponential(4, 3);
 
-  expect( result ).toEqual( 64 );
+  expect(result).toEqual(64);
 });
 
-test('exponential - 2', () => {
-  const result = exponential( 5, 2 );
+test("exponential - 2", () => {
+  const result = exponential(5, 2);
 
-  expect( result ).toEqual( 25 );
+  expect(result).toEqual(25);
 });
 
-test('smaller - 1', () => {
-  const result = smaller( 23, 22 );
+test("smaller - 1", () => {
+  const result = smaller(23, 22);
 
-  expect( result ).toEqual( 22 );
+  expect(result).toEqual(22);
 });
 
-test('smaller - 2', () => {
-  const result = smaller( 11, 19 );
+test("smaller - 2", () => {
+  const result = smaller(11, 19);
 
-  expect( result ).toEqual( 11 );
+  expect(result).toEqual(11);
 });
 
-test('smaller - 3', () => {
-  const result = smaller( 13, 13 );
+test("smaller - 3", () => {
+  const result = smaller(13, 13);
 
-  expect( result ).toEqual( false );
+  expect(result).toEqual(false);
 });
 
-test('circumference - 1', () => {
-  const result = circumference( 5 );
+test("circumference - 1", () => {
+  const result = circumference(5);
 
-  expect( result ).toEqual( '15.708' );
+  expect(result).toEqual("15.708");
 });
 
-test('circumference - 2', () => {
-  const result = circumference( 11 );
+test("circumference - 2", () => {
+  const result = circumference(11);
 
-  expect( result ).toEqual( '34.558' );
+  expect(result).toEqual("34.558");
 });
 
-test('countOnes - 1', () => {
-  const result = countOnes( '101' );
+test("countOnes - 1", () => {
+  const result = countOnes("101");
 
-  expect( result ).toEqual( 2 );
+  expect(result).toEqual(2);
 });
 
-test('countOnes - 2', () => {
-  const result = countOnes( '01010101' );
+test("countOnes - 2", () => {
+  const result = countOnes("01010101");
 
-  expect( result ).toEqual( 4 );
+  expect(result).toEqual(4);
 });
 
-test('dateFormat - 1', () => {
-  const result = dateFormat( '03-11-2018' );
+test("dateFormat - 1", () => {
+  const result = dateFormat("03-11-2018");
 
-  expect( result ).toEqual( '3 November 2018' );
+  expect(result).toEqual("3 November 2018");
 });
 
-test('dateFormat - 2', () => {
-  const result = dateFormat( '12-03-2017' );
+test("dateFormat - 2", () => {
+  const result = dateFormat("12-03-2017");
 
-  expect( result ).toEqual( '12 March 2017' );
+  expect(result).toEqual("12 March 2017");
 });
 
-test('multiplyAll - 1', () => {
-  const result = mulitplyAll( [1, 2, 3] );
+test.only("multiplyAll - 1", () => {
+  const result = mulitplyAll([1, 2, 3]);
 
-  expect( result ).toEqual( 6 );
+  expect(result).toEqual(6);
 });
 
-test('multiplyAll - 2', () => {
-  const result = mulitplyAll( [3, 5, 2] );
+test.only("multiplyAll - 2", () => {
+  const result = mulitplyAll([3, 5, 2]);
 
-  expect( result ).toEqual( 30 );
+  expect(result).toEqual(30);
 });
 
-test('largestArea - 1', () => {
-  const result = largestArea( [[4,2], [7, 1], [5, 5]] );
+test("largestArea - 1", () => {
+  const result = largestArea([[4, 2], [7, 1], [5, 5]]);
 
-  expect( result ).toEqual( 25 );
+  expect(result).toEqual(25);
 });
 
-test('largestArea - 2', () => {
-  const result = largestArea( [[2,2], [2, 12], [3, 7], [8, 3]] );
+test("largestArea - 2", () => {
+  const result = largestArea([[2, 2], [2, 12], [3, 7], [8, 3]]);
 
-  expect( result ).toEqual( 24 );
+  expect(result).toEqual(24);
 });
